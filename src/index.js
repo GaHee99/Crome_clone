2d9c3bcf6dcd922d401f75a15ddd097fdddfb17b
GaHee99 <77230391+GaHee99@users.noreply.github.com>
2021-11-24 18:02:45 +0900

    crome_clone_11_mine

--- src/index.js
+++ src/index.js
@@ -1,1 +1,19 @@
 const clockTitle = document.querySelector('.js-clock')
+
+function getClock() {
+  const today = new Date()
+  const dday = new Date(2021, 12, 25)
+  const gap = dday.getTime() - today.getTime()
+  const day = Math.floor(gap / (1000 * 60 * 60 * 24))
+  const hour = String(
+    Math.floor((gap % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)),
+  ).padStart(2, '0')
+  const min = String(
+    Math.floor((gap % (1000 * 60 * 60)) / (1000 * 60)),
+  ).padStart(2, '0')
+  const sec = String(Math.floor((gap % (1000 * 60)) / 1000)).padStart(2, '0')
+
+  clockTitle.innerText = `${day}d ${hour}h ${min}m ${sec}s`
+}
+getClock()
+setInterval(getClock, 1000)
